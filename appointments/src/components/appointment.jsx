c465fc51f26d2feb57467ca311155743761a9f22
maxheim <milony.h@googlemail.com>
2020-09-28 22:14:51 +0200

    [TASK] add state to hold data for component

--- appointments/src/components/appointment.jsx
+++ appointments/src/components/appointment.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 
 const appointmentTimeOfDay = startsAt => {
     const [h, m] = new Date(startsAt).toTimeString().split(':');
@@ -14,22 +15,24 @@ export const Appointment = ({customer}) => {
 }
 
 export const AppointmentsDayView = ({appointments}) => {
+
+    const [selectedAppointment, setSelectedAppointment] = useState(0);
+
     return (
         <div id="appointmentsDayView">
             <ol>
-                {appointments.map((appointment) => {
+                {appointments.map((appointment, index) => {
                     return (
                         <li key={appointment.startsAt}>
-                            <button type="button">
+                            <button type="button" onClick={()=> setSelectedAppointment(index)}>
                                 {appointmentTimeOfDay(appointment.startsAt)}
                             </button>
-                        </li>
-                    );
+                        </li>);
                 })}
             </ol>
-            {appointments.length === 0 ?(
-                <p>There are no appointments scheduled for today.</p>)
-                : (<Appointment {...appointments[0]}/>)
+            {appointments.length === 0 ? (
+                    <p>There are no appointments scheduled for today.</p>)
+                : (<Appointment {...appointments[selectedAppointment]}/>)
             }
         </div>
     );
